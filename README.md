43f62f498852b6fbcdbe8554fe792f6fa8217f84
uos-bhkim <shdik2002@uos.ac.kr>
2022-10-25 16:08:31 +0900

    Add initial form of cityscapes dataset

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # torch-semantic-seg
+
+## To dos 
+### 1. create data pipelines
+#### &emsp; 1. Load Image and annotation
+#### &emsp; 2. Simple augmentations (FlipLR, FlipUD)
+
+### 2. create datasets 
+#### &emsp; 1. cityscapes style dataset 
+
+### 3. create models
+#### &emsp; 1. Encoders (Backbones) 
+#### &emsp; 2. Decoders (Neck)
+#### &emsp; 3. Segmentation Head  
+
+<br />
+<br />
+
+# Referneces 
+#### [1] [PyTorch - WRITING CUSTOM DATASETS, DATALOADERS AND TRANSFORMS](https://pytorch.org/tutorials/beginner/data_loading_tutorial.html)
---
+++ datasets/cityscapes.py
@@ -0,0 +1,71 @@
+import cv2
+
+import os.path as osp 
+
+from glob import glob 
+from torch.utils.data import Dataset, DataLoader
+
+"""
+References: 
+
+[1] https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+"""
+
+class CityscapesDataset(Dataset):
+    """Cityscapes dataset."""
+
+    def __init__(self, root_dir, split='train', transform=None):
+        """
+        Args: 
+            root_dir (str): Directory with all the images.
+            split (str): The dataset split, supports 'train', 'val', or 'test'
+
+        Folder structure:
+            root_dir 
+                └leftImg8bit
+                    └ train/val/test
+                        └ cities 
+                            └ ***_leftImg8bit.png
+                └gtFine
+                    └ train/val/test
+                        └ cities
+                            └ ***_gtFine_labelTrainIds.png
+                
+        """
+        
+        self.root_dir = root_dir
+        self.transform = transform
+        self.split = split
+
+        self.img_list = glob(osp.join(self.root_dir, 'leftImg8bit', self.split, '*', '*_leftImg8bit.png'))
+
+    
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        """
+        Args: 
+            idx (int): data index
+
+        Returns:
+            img (np.arr) 
+            gt (np.arr) 
+        
+        """
+
+        img_path = self.img_list[idx]
+        gt_path = img_path.replace('_leftImg8bit.png', '_gtFine_labelTrainIds.png')
+        gt_path = gt_path.replace('/leftImg8bit', '/gtFine')
+        
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        gt = cv2.imread(gt_path, cv2.IMREAD_UNCHANGED)
+        
+        return img, gt 
+
+
+
+            
+
+            
