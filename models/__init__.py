ff8b0a878c41661516ceb1355329abb553d0679c
uos-bhkim <shdik2002@uos.ac.kr>
2022-11-08 16:49:02 +0900

    Add lr_schedulers.py and polynomialLR

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 from .wrapper import * 
 from .losses import * 
 from .heads import * 
+from .lr_schedulers import * 
 
 __all__ = [
-    'ModelWrapper', 'CrossEntropyLoss'
+    'ModelWrapper', 'CrossEntropyLoss', 'PolynomialLR'
 ]
