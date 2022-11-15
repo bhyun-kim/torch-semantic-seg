098c347c58232b59bdc5c4a49aca123f95b3ef90
uos-bhkim <shdik2002@uos.ac.kr>
2022-11-15 10:13:52 +0900

    Refactor evaluate.py

--- tools/evaluate.py
+++ tools/evaluate.py
@@ -1,12 +1,13 @@
 import torch 
+import numpy as np
+
 from tqdm import tqdm
+from prettytable import PrettyTable
+
 
 def evaluate(model, dataloader, device, logger, ignore_idx=255.):
     # again no gradients needed
     model.eval()
-
-    import numpy as np
-    from prettytable import PrettyTable
 
     if hasattr(dataloader.dataset, 'datasets'):
         # if dataset is concat, bring the num of classes of the first
@@ -17,50 +18,95 @@ def evaluate(model, dataloader, device, logger, ignore_idx=255.):
         num_classes = len(classes)
     
     with torch.no_grad():
-        category_vectors = []
+        preds = []
+        gts = []
         for data in tqdm(dataloader, desc='Evaluation:'):
+
             inputs, labels = data['image'], data['segmap']
-
             inputs, labels = inputs.to(device), labels.to(device)
+
             outputs = model(inputs)
-            predictions = torch.argmax(outputs, dim=1).flatten()
-            labels = labels.flatten()
-            predictions = predictions[labels != ignore_idx]
-            labels = labels[labels != ignore_idx]
-            
-            category_vector = predictions * num_classes + labels
-            category_vector = category_vector.detach().cpu().numpy()
 
-            category_vectors.append(category_vector)
+            preds.append(torch.argmax(outputs, dim=1))
+            gts.append(labels)
 
-        category_vectors = np.concatenate(category_vectors)
-        category_bincount = np.bincount(category_vectors, minlength = num_classes ** 2)
+        cm = calculate_cm(preds, gts, num_classes, ignore_idx)
 
-        confusion_matrix = np.zeros((num_classes, num_classes), dtype=np.int64)
+        calculate_miou(cm, logger=logger, classes=classes) 
+
+    model.train()
+
+
+def calculate_cm(preds, gts, num_classes, ignore_idx):
+    """calculate confusion matrix
+    Args:
+        preds (list): list of predictions (torch.tensor)
+        gts (list): list of ground truths (torch.tensor)
+        num_classes (int)
+        ignore_idx (int)
+
+    Returns: 
+        cm (np.array): confusion matrix of which shape is (num_classes x num_classes)
+
+    """
+    
+    category_vectors = []
+    for pred, gt in zip(preds, gts):
         
-        for i in range(num_classes ** 2):
-            row, col = i // num_classes, i % num_classes
-            confusion_matrix[row, col] = category_bincount[i] 
+        pred = pred.flatten().detach().cpu().numpy()
+        gt = gt.flatten().detach().cpu().numpy()
 
-        ious = []
-        for i in range(num_classes): 
-            intersection = confusion_matrix[i, i]
-            union = np.sum(confusion_matrix[i, :]) + np.sum(confusion_matrix[:, i]) - confusion_matrix[i, i]
-            ious.append(intersection / union)
+        pred = pred[gt != ignore_idx]
+        gt = gt[gt != ignore_idx]
         
-        miou = np.mean(ious)
+        category_vector = gt * num_classes + pred
+        category_vector = category_vector.detach().cpu().numpy()
 
-        iou_table = PrettyTable(['Class', 'IoU (%)'])
-        
-        for idx, iou in enumerate(ious):
-            iou_table.add_row([classes[idx], f'{iou*100:.2f}'])
+        category_vectors.append(category_vector)
 
-        miou_table = PrettyTable(['Metric', '(%)' ])
-        miou_table.add_row(['mIoU', f'{miou*100:.2f}'])
+    category_vectors = np.concatenate(category_vectors)
+    category_bincount = np.bincount(category_vectors, minlength = num_classes ** 2)
+
+    cm = category_bincount.reshape(num_classes, num_classes)
+
+    return cm
+
+def calculate_miou(cm, logger=None, classes=None):
+    """calculate miou
+    Args: 
+        cm (np.array): confusion matrix of which shape is (num_classes x num_classes)
+        logger (logger): default = None, If logger is None, print mIoU on terminal 
+    """
+    assert cm.shape[0] == cm.shape[1], "The number of columns and rows of confusion matrix should be same."
+
+    num_classes = cm.shape[0]
+
+    if classes == None: 
+        classes = [f'class_{i}' for i in range(num_classes)]
+
+    ious = []
+    for i in range(num_classes): 
+        intersection = cm[i, i]
+        union = cm[i, :].sum() + cm[:, i].sum() - intersection
+        ious.append(intersection / union)
+    
+    miou = np.mean(ious)
+
+    iou_table = PrettyTable(['Class', 'IoU (%)'])
+    
+    for idx, iou in enumerate(ious):
+        iou_table.add_row([classes[idx], f'{iou*100:.2f}'])
+
+    miou_table = PrettyTable(['Metric', '(%)' ])
+    miou_table.add_row(['mIoU', f'{miou*100:.2f}'])
+
+
+    if logger: 
 
         logger.info('\n'+iou_table.get_string())
         logger.info('\n'+miou_table.get_string())
+    else: 
+        print('\n'+iou_table.get_string())
+        print('\n'+miou_table.get_string())
         
-            
-    model.train()
-    return miou, ious
+
