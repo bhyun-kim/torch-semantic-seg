1108d7e9c7c771e328f606fccf65a89983b233f8
bhyun-kim <kimbh.mail@gmail.com>
2022-11-23 15:12:29 +0900

    Add Logger file

--- tools/utils.py
+++ tools/utils.py
@@ -1,4 +1,9 @@
 
+import logging
+import os
+from datetime import datetime
+
+
 def cvt_pathToModule(file_path):
     """Convert path (string) to module form.
     
@@ -31,3 +36,47 @@ def cvt_moduleToDict(mod) :
         }
     
     return cfg
+
+class Logger(object):
+    def __init__(self, directory, verbose=1):
+        """
+        Args:
+            directory (str): path to write logging file 
+            verbose (int): 
+                if verbose == 1: logger print and write on logging files
+                if verbose == -1: skips both print and write  
+        """
+
+        # build logger  
+        self.verbose = verbose
+
+        if self.verbose != -1:
+
+            logging.basicConfig(level=logging.INFO)
+            self.logger = logging.getLogger()
+            self.logger.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(message)s')
+            os.makedirs(directory, exist_ok=True)
+            
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            log_file = directory + f'/{current_time}.log'
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
+            self.logger.info('Log file is %s' % log_file)
+
+    def info(self, input):
+        """
+        Args: 
+            input (str)
+        """
+        print(input)
+        if self.verbose == 1:
+            self.logger.info(input)
+
+        elif self.verbose == -1:
+            pass
+
+        return None
+
